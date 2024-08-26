4b311b279d9f9a24b223b7225c47c787ff141679
morpheus5162 <sampbaguio@gmail.com>
2024-08-26 23:08:40 +0800

    Reapply "Edits"
    This reverts commit cfc0a1c4c9541558b60d4199434af99d25abad89.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -19,12 +19,9 @@ namespace ASP.Net_Final_Project
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
